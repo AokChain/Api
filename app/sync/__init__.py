e59269b332759068a4a473f260593b26d24bfd76
volbil <volbilnexus@gmail.com>
2026-06-10 19:04:44 +0300

    Added ipfs to sync

--- app/sync/__init__.py
+++ app/sync/__init__.py
@@ -1,4 +1,9 @@
-from .chain import sync_chain
 from .mempool import sync_mempool
+from .chain import sync_chain
+from .ipfs import sync_ipfs
 
-__all__ = ["sync_chain", "sync_mempool"]
+__all__ = [
+    "sync_mempool",
+    "sync_chain",
+    "sync_ipfs",
+]
